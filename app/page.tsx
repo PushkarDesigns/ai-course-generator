166cc14e677bee5010980047583b9afe9a9db9d9
Pushkar <rawatprince815@gmail.com>
2026-07-09 15:28:11 +0530

    Project setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2 className="">hello</h2>
+      <Button>click Me</Button>
+    </div>
+  );
+}
