f157190117544be77e95e3067054ba851676d895
Pushkar <rawatprince815@gmail.com>
2026-07-14 21:41:43 +0530

    working

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
+import Provider from "./provider";
+// import { cn } from "@/lib/utils";
 
 const AppFont = DM_Sans({ subsets: ["latin"] });
 
@@ -32,7 +33,11 @@ export default function RootLayout({
     <>
       <ClerkProvider>
         <html lang="en" className={AppFont.className}>
-          <body className="min-h-full flex flex-col">{children}</body>
+          <body className="min-h-full flex flex-col">
+            <Provider>
+              {children}
+            </Provider>
+          </body>
         </html>
       </ClerkProvider>
     </>
---
+++ app/provider.tsx
@@ -0,0 +1,17 @@
+"use client"
+import React from 'react'
+
+function Provider({ children }: { children: React.ReactNode }) {
+    
+    const CreateNewUser = () => {
+    // user API endpoint call to create a new user
+}
+
+
+
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default Provider
